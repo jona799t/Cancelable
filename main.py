9dbdeed68b89425a8ae5ccbd3622e40adb24ec51
jona799t <70392256+jona799t@users.noreply.github.com>
2022-01-14 16:02:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def cancelableSleep(seconds):
+    endSleep = time.time() + seconds
+    while True:
+        if shouldCancelSleep:
+            break
+        if time.time() >= endSleep:
+            break
+
+#Lav det til et module som også understøtter input()
+#Det skal bruges til programmer hvor man skal kunne annullere en thread
